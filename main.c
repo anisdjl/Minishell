657cbe74527ffd59844919001653f9579e56c3c7
voltron94 <voltrongaming93@gmail.com>
2026-02-11 16:19:24 +0100

    v0.1

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: adjelili <adjelili@student.42.fr>          +#+  +:+       +#+        */
+/*   By: volt <volt@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/11 12:57:30 by adjelili          #+#    #+#             */
-/*   Updated: 2026/02/11 16:09:42 by adjelili         ###   ########.fr       */
+/*   Updated: 2026/02/11 16:16:55 by volt             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,15 @@ int main(void)
 	int fd;
 	
 	fd = open(".minishell_history", O_RDWR | O_CREAT | O_APPEND, 0644);
-	line = readline("minishell> ");
-	printf("%s\n", line);
-	if (line)
+	while (1)
 	{
-		write(fd, line, strlen(line));
-		write(fd, "\n", 1);
+		line = readline("minishell> ");
+		printf("%s\n", line);
+		if (line)
+		{
+			write(fd, line, strlen(line));
+			write(fd, "\n", 1);
+		}
 	}
 	return (0);
 }
