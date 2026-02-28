a9ce2788747fd667258143ec48c72c0d40e863f9
anisdjl <adjelili954@gmail.com>
2026-02-28 22:38:08 +0100

    added some useful func for the exec

--- exec/exec.h
+++ exec/exec.h
@@ -1,29 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   env.h                                              :+:      :+:    :+:   */
+/*   exec.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: anis <anis@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/02/26 16:43:34 by adjelili          #+#    #+#             */
-/*   Updated: 2026/02/28 22:28:38 by anis             ###   ########.fr       */
+/*   Created: 2026/02/28 22:25:29 by anis              #+#    #+#             */
+/*   Updated: 2026/02/28 22:29:02 by anis             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef ENV_H
-# define ENV_H
+#ifndef EXEC_H
+# define EXEC_H
 
-# include "../minishell.h"
+#include "../minishell.h"
+#include "../env/env.h"
 
-typedef struct s_env
-{
-	char			*key;
-	char			*value;
-	struct s_env	*next;	
-}	t_env;
-
-void	ft_lstadd_back_env(t_env **lst, t_env *new_env);
-int 	get_env(char **envp);
-char	*create_key(char *envp);
+int		ft_lstsize_env(t_env *lst);
+int		exec(t_tree *ast, t_env *env);
+int		handle_pipes(t_tree *node, t_env *env);
+int		exec_cmd(t_tree *node, t_env *env);
+char 	**env_to_tab(t_env **env);
 
 #endif
