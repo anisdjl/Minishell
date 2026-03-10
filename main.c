521c4ab0173b7035ec718e04ebce07462316ef4f
anisdjl <adjelili954@gmail.com>
2026-03-10 14:29:37 +0100

    added the quotes

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: adjelili <adjelili@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/24 12:57:30 by adjelili          #+#    #+#             */
-/*   Updated: 2026/03/10 11:48:31 by adjelili         ###   ########.fr       */
+/*   Updated: 2026/03/10 14:26:03 by adjelili         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,9 @@ int main(int argc, char **argv, char **envp)
         if (!check_parentheses(line) || !check_quotes(line))
             continue;
         lexer = ft_lexer(line);
-		env->exit_status->exit_status = exec(AST_launcher(lexer->content), env);
+		debug_tokens(&lexer->content);
+		AST_launcher(lexer->content);
+		//exec(AST_launcher(lexer->content), env);
 		printf("%d\n", env->exit_status->exit_status);
         ft_free_all_malloc();
 		//free env
